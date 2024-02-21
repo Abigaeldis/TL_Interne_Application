48d70f16f65a477b4ac1d26891f46549fcfedbae
Abigaeldis <abigael.disegna@gmail.com>
2024-02-21 11:40:43 +0100

    dao plat

---
+++ src/main/java/com/m2i/TL_Interne_Application/repositories/PlatRepository.java
@@ -0,0 +1,7 @@
+package com.m2i.TL_Interne_Application.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.m2i.TL_Interne_Application.entities.Plat;
+
+public interface PlatRepository extends CrudRepository<Plat, Integer> {}
