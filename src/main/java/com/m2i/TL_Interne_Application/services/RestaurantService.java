c1a3ff82eef4f8107d756e41652f218fbeb0a2bf
RodiesATC <wxcoprozent@gmail.com>
2024-02-23 16:26:52 +0100

    fix_restaurant_saveUpdate

--- src/main/java/com/m2i/TL_Interne_Application/services/RestaurantService.java
+++ src/main/java/com/m2i/TL_Interne_Application/services/RestaurantService.java
@@ -19,9 +19,46 @@ public class RestaurantService {
 		return restaurantRepository.findById(id).get();
 	}
 
-	public void saveOrUpdate(Restaurant restaurant) throws BLLException {
+	public void save (Restaurant restaurant) throws BLLException {
 
 		BLLException blleException = new BLLException();
+
+		if (restaurant.getNom().length() < 2) {
+			blleException.ajouterErreur("Le nom doit faire au moins 2 caractères");
+		}
+
+		if (restaurant.getNom().length() > 30) {
+			blleException.ajouterErreur("Le nom doit faire au maximum 30 caractères");
+		}
+
+		if (restaurant.getAdresse().length() < 2) {
+			blleException.ajouterErreur("L'adresse doit faire au moins 2 caractères");
+		}
+
+		if (restaurant.getAdresse().length() > 150) {
+			blleException.ajouterErreur("L'adresse doit faire au maximum 150 caractères");
+		}
+
+		if (restaurant.getDescription().length() < 2) {
+			blleException.ajouterErreur("La description doit faire au moins 2 caractères");
+		}
+
+		if (restaurant.getDescription().length() > 300) {
+			blleException.ajouterErreur("La description doit faire au maximum 300 caractères");
+		}
+
+		if (blleException.getErreurs().size() > 0) {
+			throw blleException;
+		}
+
+		restaurantRepository.save(restaurant);
+	}
+	
+	public void update (int id, Restaurant restaurant) throws BLLException {
+
+		BLLException blleException = new BLLException();
+		
+		restaurant.setId(id);
 
 		if (restaurant.getNom().length() < 2) {
 			blleException.ajouterErreur("Le nom doit faire au moins 2 caractères");
