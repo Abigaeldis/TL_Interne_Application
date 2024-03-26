4f0318984da459f9fb8414d1e943df777aee1c31
Lauriane <52917786+lgascon@users.noreply.github.com>
2024-03-26 14:04:35 +0100

    ajour crossorigin sur les controllers manquants

--- src/main/java/com/m2i/controllers/PlatController.java
+++ src/main/java/com/m2i/controllers/PlatController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.m2i.services.BLLException;
 import com.m2i.services.PlatService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/plats")
 public class PlatController {
 	private PlatService platService;
